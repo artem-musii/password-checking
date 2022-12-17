c8ea2852e35710ef5f88dc4325e0d9ab6eaa40a2
Artem Musii <musiy.artem@gmail.com>
2022-12-17 21:40:59 +0200

    password-checking

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+/* eslint-disable max-len */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+
+export class AppComponent {
+  easyRegex = /^(?:[\d]*$)|(?:[a-zA-Z]*$)|(?:[-!@#%^&*()_+=[\]{}|\\:;"'<>,.?/]*$)/;
+
+  mediumRegex = /^(?:(?=.*[a-zA-Z])(?=.*[-!@#%^&*()_+=[\]{}|\\:;"'<>,.?/]).*$)|(?:(?=.*[a-zA-Z])(?=.*[\d]).*$)|(?:(?=.*[\d])(?=.*[-!@#%^&*()_+=[\]{}|\\:;"'<>,.?/]).*$)/;
+
+  strongRegex = /^(?=.*[a-zA-Z])(?=.*[\d])(?=.*[-!@#%^ &*()_+=[\]{}|\\:;"'<>,.?/]).*$/;
+
+  passwordStrength = '';
+  password = '';
+
+  checkPasswordStrength(value: string) {
+    this.password = value;
+
+    if (this.easyRegex.test(value)) {
+      this.passwordStrength = 'easy';
+    }
+
+    if (this.mediumRegex.test(value)) {
+      this.passwordStrength = 'medium';
+    }
+
+    if (this.strongRegex.test(value)) {
+      this.passwordStrength = 'strong';
+    }
+
+    if (value.trim().length < 8) {
+      this.passwordStrength = 'short';
+    }
+
+    if (!value.trim()) {
+      this.passwordStrength = '';
+    }
+  }
+}
